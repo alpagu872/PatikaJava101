d28c0980be79b40f76e461acebf16bff6cc4a6b8
Alpagu E <83309872+alpagu872@users.noreply.github.com>
2022-04-13 07:30:55 +0300

    7:30 AM UPDATE

---
+++ src/Diziler/ArraysSinifi.java
@@ -0,0 +1,8 @@
+package Diziler;
+
+public class ArraysSinifi {
+    public static void main(String[] args) {
+
+
+    }
+}
--- src/Diziler/IkiBoyutluDiziler.java
+++ src/Diziler/IkiBoyutluDiziler.java
@@ -3,17 +3,26 @@ package Diziler;
 public class IkiBoyutluDiziler {
     public static void main(String[] args) {
 
-        int[] list = new int[5];
-        int[][] matris = {
-                {0,453,939,243,783,891},
-                {453,0,490,384,620,439},
-                {939,490,0,839,863,423},
-                {243,384,839,0,544,823},
-                {783,620,863,544,0,1045},
-                {893,439,423,823,1045,0}
-        };
+        int[][] matris = new int[3][4];
+        int number = 1;
 
-        System.out.println(matris[4][5]);
+        for (int i = 0; i < matris.length; i++) {
+
+            // matris[i]
+            // matris[0][1]
+            for (int j = 0 ; j < matris[i].length; j++){
+                matris[i][j] = number++;
+
+            }
+
+        }
+
+        for (int k = 0 ; k < matris.length ; k++){
+            for (int j = 0 ; j < matris.length+1     ; j++){
+                System.out.print(matris[k][j]+ " ");
+            }
+            System.out.println();
+        }
 
 
     }
