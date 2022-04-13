de18fbb43bd63b60ef80b7a8c963ed48e89ff68c
Alpagu E <83309872+alpagu872@users.noreply.github.com>
2022-04-13 08:07:56 +0300

    8:07 AM UPDATE

--- src/Diziler/ArraysSinifi.java
+++ src/Diziler/ArraysSinifi.java
@@ -1,8 +1,26 @@
 package Diziler;
 
+import java.util.Arrays;
+
 public class ArraysSinifi {
+
     public static void main(String[] args) {
+        int[] list = {1,2,3,4,5,6,7};
+
+
+        int[] list2 = {2,6,5,8,54,6,8,9,456,54,6};
+
+        double[] list3 = {1.1,4.6,7.5,2.6};
+        Arrays.fill(list,2,7,10);
+
+        int[] list4 = {5,6,564,646,84,7,8,78978,5460};
+        Arrays.sort(list4);
+       // System.out.println(Arrays.toString(list4));
+
+        System.out.println(HelperArray.search(list4,646));
+
 
 
     }
+
 }
---
+++ src/Diziler/HelperArray.java
@@ -0,0 +1,34 @@
+package Diziler;
+
+public class HelperArray {
+
+    //static metodlara nesne oluşturmadan erişebiliriz.
+
+    static void print(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.print("]");
+
+    }
+
+    static int[] fill(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = value;
+        }
+        return arr;
+
+    }
+
+    static int search(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                return i;
+
+            }
+        }
+        return -1;
+    }
+
+}
